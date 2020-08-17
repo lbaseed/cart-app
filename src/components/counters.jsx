c95aaf760f94b3953750282428c0fb7b83fcde7a
unknown <lbaseedt@gmail.com>
2020-08-17 01:33:15 +0100

    done with cart

--- src/components/counters.jsx
+++ src/components/counters.jsx
@@ -2,31 +2,22 @@ import React, { Component } from "react";
 import Counter from "./counter";
 
 class Counters extends Component {
-  state = {
-    counters: [
-      { id: 1, value: 4 },
-      { id: 2, value: 0 },
-      { id: 3, value: 7 },
-      { id: 4, value: 0 },
-      { id: 5, value: 0 },
-    ],
-  };
-
-  handleDelete = (counterId) => {
-    const counters = this.state.counters.filter((c) => c.id !== counterId);
-    this.setState( {counters: counters} );
-  }; 
-
   render() {
+    const { onReset, counters, onDelete, onIncrement} = this.props
     return (
       <div>
-        {this.state.counters.map((counter) => (
+        <button
+          onClick={onReset}
+          className="btn btn-primary btn-sm m-2"
+        >
+          Reset
+        </button>
+        {counters.map((counter) => (
           <Counter
             key={counter.id}
-            onDel={this.handleDelete}
-            value={counter.value}
-            selected={true}
-            id={counter.id}
+            onDel={onDelete}
+            onIncrement={onIncrement}
+            counter={counter}
           />
         ))}
       </div>
